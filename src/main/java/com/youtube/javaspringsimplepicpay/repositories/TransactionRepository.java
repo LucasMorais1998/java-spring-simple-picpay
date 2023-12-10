9f26f113fb30feca7ffea2b0b478b0053a2e792a
Lucas Morais <lucas14.morais@gmail.com>
2023-12-10 16:53:21 -0300

    feat(repository): add TransactionRepository

---
+++ src/main/java/com/youtube/javaspringsimplepicpay/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.youtube.javaspringsimplepicpay.repositories;
+
+import com.youtube.javaspringsimplepicpay.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository  extends JpaRepository<Transaction, Long> {
+}
