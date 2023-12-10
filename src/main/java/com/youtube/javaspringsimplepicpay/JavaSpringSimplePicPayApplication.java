8d3693cd8b964fdb86195f8b02902704a0784605
Lucas Morais <lucas14.morais@gmail.com>
2023-12-10 14:04:37 -0300

    chore: initialize project

---
+++ src/main/java/com/youtube/javaspringsimplepicpay/JavaSpringSimplePicPayApplication.java
@@ -0,0 +1,13 @@
+package com.youtube.javaspringsimplepicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringSimplePicPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringSimplePicPayApplication.class, args);
+	}
+
+}
