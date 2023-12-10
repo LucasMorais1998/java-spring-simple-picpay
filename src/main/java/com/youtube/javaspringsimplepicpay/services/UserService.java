b54e9196be7c7eefb4fff9d200dad363e9e4876c
Lucas Morais <lucas14.morais@gmail.com>
2023-12-10 17:06:34 -0300

    feat(service): add UserService

---
+++ src/main/java/com/youtube/javaspringsimplepicpay/services/UserService.java
@@ -0,0 +1,34 @@
+package com.youtube.javaspringsimplepicpay.services;
+
+import com.youtube.javaspringsimplepicpay.domain.user.User;
+import com.youtube.javaspringsimplepicpay.domain.user.UserType;
+import com.youtube.javaspringsimplepicpay.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransactiion(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Uusuário do tipo Lojista não está autorizado a relazar transação.");
+        }
+
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return  this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
+}
